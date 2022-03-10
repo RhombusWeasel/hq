5d696f5bb57b0ae47304cfba8ed6852f585de704
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-10 23:18:32 +0000

    Potions 0.1.36

--- module/hq.js
+++ module/hq.js
@@ -87,7 +87,7 @@ Hooks.once("init", function () {
 Hooks.on("ready", () => {
     console.log("HQ | Initializing socket listeners...")
     game.socket.on(`system.hq`, (data) => {
-        console.log('RECIEVE:', data.operation, data.data);
+        console.log('RECEIVE:', data.operation, data.data);
         if (data.operation in hq.socket.commands) {
             hq.socket.commands[data.operation](data.data);
             hq.gm.update_sheet();
