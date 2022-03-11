58b330845488dc047d94ee637d62d8eb074deb14
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-11 16:36:34 +0000

    Potions 0.2.25

--- module/hq.js
+++ module/hq.js
@@ -41,7 +41,6 @@ Hooks.once("init", function () {
     });
 
     Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
-        console.log(v1, operator, v2, options);
         switch (operator) {
             case '==':
                 return (v1 == v2) ? options.fn(this) : options.inverse(this);
