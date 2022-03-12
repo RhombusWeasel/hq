ea4e11a033ec045c3a1b1f90e7fc3f8d24fd0925
RhombusWeasel <RhombusWeasel@gmail.com>
2022-03-12 11:12:49 +0000

    Potions 0.3.6

--- module/sheets/evil_wizard.js
+++ module/sheets/evil_wizard.js
@@ -25,6 +25,9 @@ export default class hqGMSheet extends ActorSheet {
 
   activateListeners(html) {
     html.find(".new-game").click(this._on_new_game.bind(this));
+    html.find(".reset-treasure").click(this._on_reset_treasure.bind(this));
+    html.find(".next-turn").click(this._on_next_turn.bind(this));
+    html.find(".next-round").click(this._on_next_round.bind(this));
     
     //html.find(".class-select").change(this._on_class_select.bind(this));
     return super.activateListeners(html);
@@ -35,6 +38,27 @@ export default class hqGMSheet extends ActorSheet {
     game.hq.treasure_deck = hq.deck.shuffle(hq.deck.new('treasure'));
     hq.journal.save('treasure_deck', game.hq.treasure_deck);
     hq.socket.emit('new_game', {});
+  }
+
+  _on_reset_treasure(ev) {
+    game.hq.treasure_deck = hq.deck.shuffle(hq.deck.new('treasure'));
+    hq.journal.save('treasure_deck', game.hq.treasure_deck);
+  }
+
+  _on_next_turn(ev) {
+    let heros = hq.gm.get_online_actors().sort((a, b) => {return hq.sort.compare(a, b, 'initiative')});
+    for (let i = 0; i < heros.length; i++) {
+      const hero = heros[i];
+      if (hero.data.data.attack_taken == false && hero.data.data.move_rolled == false) {
+        hq.socket.emit('next_turn', {name: hero.name});
+        return;
+      }
+    }
+    hq.dialog.prompt('Monster Turn', 'It is time for the monsters to act.');
+  }
+
+  _on_next_round(ev) {
+
   }
 
 }
